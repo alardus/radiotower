f9bf6908552baa3cfade1a675f5ed6d5664741b7
Danil Ivanov <mash@yandex-team.ru>
2014-04-04 16:11:39 +0400

    Listening proxy settings and change status. Formatting code.

--- popup.js
+++ popup.js
@@ -1,58 +1,48 @@
 function enable() {
-      var config = {
+    var config = {
         mode: "pac_script",
         pacScript: {
-          url: "http://portaller.com/proxy.pac"
+            url: "http://portaller.com/proxy.pac"
         }
-      };
-      chrome.proxy.settings.set(
-          {value: config, scope: 'regular'},
-          function() {});
+    };
+
+    chrome.proxy.settings.set(
+        { value: config, scope: "regular"},
+        function() {});
 }
-      
 
 function disable() {
-      var config = {
+    var config = {
         mode: "system"
-      };
-      chrome.proxy.settings.set(
-          {value: config, scope: 'regular'},
-          function() {});
-      
-}
+    };
 
-
-function main() {
+    chrome.proxy.settings.set(
+        { value: config, scope: "regular" },
+        function() {});
 
 }
 
-function textToSpan(text) {
-    // var span = document.getElementById('statusText');
-    // if (!span) {
-    //     span = document.createElement('span');
-    //     span.setAttribute('id', 'statusText');
-    //     document.body.appendChild(span);
-    // }
-    // span.textContent = text;
-
-    // Если span сразу будет в вёрстке, то код создания можно убрать и писать так,
-    // можно даже в функции status, если немного и удобнее.
-    document.getElementById('statusText').textContent = text;
+function setStatus(mode) {
+    var text = mode === "system"
+             ? "Radio Tower turned off"
+             : "Radio Tower on Air";
+    document.getElementById("statusText").textContent = text;
 }
 
+function updateStatus() {
+    chrome.proxy.settings.get(
+        { "incognito": false },
+        function(config) {
+            setStatus(config.value.mode);
+        });
+}
 
-chrome.proxy.settings.get(
-            {'incognito': false},
-            function(config) {
-                var mode = config.value.mode;
-                var text = (mode === "system")
-                         ? "Radio Tower turned off"
-                         : "Radio Tower on Air";
-                textToSpan(text);
-            });
+chrome.proxy.settings.onChange.addListener(function() {
+    updateStatus();
+});
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#enable').addEventListener('click', enable);
-  document.querySelector('#disable').addEventListener('click', disable);
-  main();
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("enable").addEventListener("click", enable);
+    document.getElementById("disable").addEventListener("click", disable);
+    updateStatus();
 });
