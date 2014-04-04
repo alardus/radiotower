ff0eb245d5b5cf7add8fba20ee4cce51cdcfb094
Danil Ivanov <danil.ivanov@gmail.com>
2014-04-04 15:25:25 +0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -26,10 +26,30 @@ function main() {
 
 }
 
+function textToSpan(text) {
+    var span = document.getElementById('statusText');
+    if (!span) {
+        span = document.createElement('span');
+        span.setAttribute('id', 'statusText');
+        document.body.appendChild(span);
+    }
+    span.textContent = text;
+
+    // Если span сразу будет в вёрстке, то код создания можно убрать и писать так,
+    // можно даже в функции status, если немного и удобнее.
+    // document.getElementById('statusText').textContent = text;
+}
+
 function status() {
   chrome.proxy.settings.get(
             {'incognito': false},
-            function(config) {console.log(JSON.stringify(config));});
+            function(config) {
+                var mode = config.value.mode;
+                var text = (mode === "system")
+                         ? "system!"
+                         : "not system";
+                textToSpan(text);
+            });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
